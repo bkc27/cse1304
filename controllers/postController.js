ab5c13f82ec2e95619b4b8856e19aa9a92007c4b
unknown <bkc.vns@gmail.com>
2026-04-21 12:36:53 +0530

    Completed

--- controllers/postController.js
+++ controllers/postController.js
@@ -29,13 +29,119 @@ const createPost = async (req, res) => {
     }
 };
 
-const getAllPost = async () => { };
+const getAllPost = async (req, res) => {
+    try {
+        const posts = await Post.find().populate("user", "name email");
 
-const getSinglePost = () => { };
+        res.status(200).json({
+            success: true,
+            message: "All Posts",
+            count: posts.length,
+            posts
+        });
+    }
+    catch (e) {
+        res.status(500).json({
+            success: true,
+            message: "Unable to retrieve post",
+            error: e.message
+        });
+    }
+};
 
-const updatePost = () => { };
+const getSinglePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await Post.findById(id);
+        if (!post) {
+            return res.status(404).json({
+                success: false,
+                message: "Post Not Found"
+            });
+        }
+        res.status(200).json({
+            success: true,
+            message: "Post Found",
+            post
+        });
+    }
+    catch (e) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to find post",
+            eror: e.message
+        })
+    }
+};
 
-const deletePost = () => { };
+const updatePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, content } = req.body;
+        const post = await Post.findById(id);
+        if (!post) {
+            return res.status(404).json({
+                success: false,
+                message: "Post Not Found"
+            });
+        }
+        if (post.user.toString() !== req.user._id.toString()) {
+            return res.status(403).json({
+                success: false,
+                message: "You can update only your post"
+            });
+        }
+
+        post.title = title || post.title;
+        post.content = content || post.content;
+        await post.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Post Updated",
+            post
+        });
+    }
+    catch (e) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to Update",
+            error: e.message
+        });
+    }
+};
+
+const deletePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await Post.findById(id);
+        if (!post) {
+            return res.status(404).json({
+                success: false,
+                message: "Post Not Found"
+            });
+        }
+        if (post.user.toString() !== req.user._id.toString()) {
+            return res.status(403).json({
+                success: false,
+                message: "You can delete only your post"
+            });
+        }
+
+        await post.deleteOne();
+        res.status(200).json({
+            success: true,
+            message: "Post Deleted"
+        });
+    }
+    catch (e) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to Delete",
+            error: e.message
+        });
+    }
+};
 
 
-module.exports = { createPost };
+module.exports = { createPost, getAllPost, getSinglePost, updatePost, deletePost };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const protect = require("../middleware/authMiddleware");
-const { createPost } = require("../controllers/postController");
+const { createPost, getAllPost, getSinglePost, updatePost, deletePost } = require("../controllers/postController");
 
 router.post("/", protect, createPost);
+router.get("/", getAllPost);
+router.get("/:id", getSinglePost);
+router.put("/:id", protect, updatePost);
+router.delete("/:id", protect, deletePost);
 
 module.exports = router;
